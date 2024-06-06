c0032c5d25a1a1d212d1a9cc145c2166802dc877
maximiliano <maxisecundaria06@gmail.com>
2024-06-05 21:44:28 -0300

    arreglo de errores programacion 3

--- controleler/student.controler.php
+++ controleler/student.controler.php
@@ -1,30 +1,23 @@
 <?php
 include("model/student.class.php");
-$operacion=$_REQUEST["operacion"];
 
-$student=new Student (); // creamos el objeto $student de tipo Student
+$operacion = $_REQUEST["operacion"];
 
+$student = new Student(); // creamos el objeto $student de tipo Student
 
-
-$operation=$_REQUEST["operation"]
-
-
-if ($operacion=="add"){
-    $student->setDni($_REQUEST["dni"])
-    $student->setName($_REQUEST["name"])
-    $student->setBirthday($_REQUEST["birthday"])
-    $student->setPhone($_REQUEST["phone"])
-    $student->setSurname($_REQUEST["surname"])
-    $student->setEmail($_REQUEST["Email"])
-    $student->setAdress($_REQUEST["Adress"])
-    $student->setSchool($_REQUEST["School"])
+if ($operacion == "add") {
+    $student->setDni($_REQUEST["dni"]);
+    $student->setName($_REQUEST["name"]);
+    $student->setBirthday($_REQUEST["birthday"]);
+    $student->setPhone($_REQUEST["phone"]);
+    $student->setSurname($_REQUEST["surname"]);
+    $student->setEmail($_REQUEST["email"]); // "Email" corregido a "email"
+    $student->setAdress($_REQUEST["adress"]); // "Adress" corregido a "adress"
+    $student->setSchool($_REQUEST["school"]); // "School" corregido a "school"
     $student->addStudent();
-
-}else if($operacion=="udate"){
-
-
-}else if($operacion=="delete")
-
-
-
+} else if ($operacion == "update") { // "udate" corregido a "update"
+    // Aquí iría la lógica para actualizar un estudiante
+} else if ($operacion == "delete") {
+    // Aquí iría la lógica para eliminar un estudiante
+}
 ?>
